8e52245841ecd1065e7c6966b65a98b24b47ec7d
Dannel Jurado <dannelj@gmail.com>
2012-12-17 03:14:38 -0500

    var twitter_js = "I hate you"

--- updater.rb
+++ updater.rb
@@ -25,15 +25,34 @@ raise ArgumentError, "Cannot look back further than 3200 tweets" if opts[:limit]
 
 count = opts[:limit]
 
+user_details_path = dir + "/data/js/user_details.js"
+
+def read_required_twitter_js_file(file_path)
+    raise "#{file_path} must exist" unless  File.exists?(file_path)
+    read_twitter_js_file(file_path)
+end
+
+def read_twitter_js_file(file_path)
+    file_contents = open(file_path).read
+    json_file_contents = file_contents.gsub(/^((var)?\s*(.+?)\s+=\s+)/m, '')
+    json = JSON.parse(json_file_contents)
+end
+
 # find user_id in data/js/user_details.js
+user_details = read_required_twitter_js_file(user_details_path)
+user_id = user_details["id"]
+screen_name = user_details["screen_name"]
+puts "Twitter Archive for #{screen_name} (##{user_id}) found"
 
 # find latest_month_file (should be last when sorted alphanumerically)
+
+
 # find :last_tweet_id in latest_month_file
 # get response from API
 # add tweets to json data file and csv data file
     # if tweets returned contain new month, create new month files, add file location to tweet_index.js 
 # add count to tweet_index.js, payload_details.js
 
-twitter_url = 'http://api.twitter.com/1/statuses/user_timeline.json?count=1000&user_id=809399&since_id=279926892881145857'
+twitter_url = "http://api.twitter.com/1/statuses/user_timeline.json?count=#{count}&user_id=#{user_id}&since_id=#{last_tweet_id}"
 
-tweets = JSON.parse(open(url).read)
+tweets = JSON.parse(open(twitter_url).read)
